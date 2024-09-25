b5234904cfbd1c383eed183d6f87088c6b3a02cd
Shukri Abdi <shukri751@gmail.com>
2024-09-26 00:46:31 +0300

    Control Flow: Conditional Statements lab done

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,59 @@
 #!/usr/bin/env python3
-
+# admin_login():
 def admin_login(username, password):
-    # your code here
-    pass
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
 
+
+print(admin_login("admin", "12345"))
+print(admin_login("admin", "123456"))
+
+
+#  hows_the_weather():
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif 40 <= temperature <= 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
 
+
+print(hows_the_weather(90))
+
+
+# fizzbuzz():
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 
+
+print(fizzbuzz(30))
+
+
+# calculator():
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+    else:
+        print("Invalid operation!")  # This ensures "Invalid operation!" is printed
+        return None
+
+
+print(calculator(10, 4, "+"))
